5cf28ccda55bdfd937cae95a7ee5c3cfe80932d1
Punna <dharmasaiteja.p@gmail.com>
2017-04-16 16:31:52 -0500

    First Commit : project setup

---
+++ src/main/java/com/fm/test/app/AwsTestAppApplication.java
@@ -0,0 +1,12 @@
+package com.fm.test.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsTestAppApplication.class, args);
+	}
+}
